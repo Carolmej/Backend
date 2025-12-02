cdcc528632b3f3e257caad3ea4bd4c4d62b83e2c
migue <miguelolvhdz@gmail.com>
2025-12-02 10:50:24 -0600

    Elimino carpeta back de main

--- back/init.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const folders = ['./reports', './projects'];
-
-folders.forEach(folder => {
-    if (!fs.existsSync(folder)) {
-      fs.mkdirSync(folder, { recursive: true });
-    }
-});
